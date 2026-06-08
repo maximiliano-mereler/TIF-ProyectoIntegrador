f986c8ababed3d07f684600b6613bb98e9c2062a
Maximiliano Ramón Merele <84930872+maximiliano-mereler@users.noreply.github.com>
2026-06-08 20:30:35 -0300

    Export enums from entities

--- backend/src/entities/index.ts
+++ backend/src/entities/index.ts
@@ -1,4 +1,4 @@
-export { Usuario } from './usuario.entity';
-export { Cliente } from './cliente.entity';
-export { Proyecto } from './proyecto.entity';
-export { Tarea } from './tarea.entity';
+export { Usuario, EstadoUsuario } from './usuario.entity';
+export { Cliente, EstadoCliente } from './cliente.entity';
+export { Proyecto, EstadoProyecto } from './proyecto.entity';
+export { Tarea, EstadoTarea } from './tarea.entity';
