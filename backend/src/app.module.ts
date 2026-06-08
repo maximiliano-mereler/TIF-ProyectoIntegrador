2da2440aee24aa3a6fcb778356519137db0f7136
Maximiliano Ramón Merele <84930872+maximiliano-mereler@users.noreply.github.com>
2026-06-08 20:17:17 -0300

    Configure TypeORM in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { databaseConfig } from './database.config';
+import { Usuario, Cliente, Proyecto, Tarea } from './entities';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(databaseConfig),
+    TypeOrmModule.forFeature([Usuario, Cliente, Proyecto, Tarea]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
