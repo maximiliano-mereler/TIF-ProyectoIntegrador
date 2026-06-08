b9c514b38e8ae052b3f78e14c3c1751e2f719029
Maximiliano Ramón Merele <84930872+maximiliano-mereler@users.noreply.github.com>
2026-06-08 20:27:31 -0300

    Import UsuariosModule in app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,11 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { databaseConfig } from './database.config';
 import { Usuario, Cliente, Proyecto, Tarea } from './entities';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(databaseConfig),
     TypeOrmModule.forFeature([Usuario, Cliente, Proyecto, Tarea]),
+    UsuariosModule,
   ],
   controllers: [AppController],
   providers: [AppService],
